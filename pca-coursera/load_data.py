ee31da9441840548af127959357558dd481dcd35
rais ilham <raisilham20@gmail.com>
2023-03-25 20:26:09 +0700

    delete some files

--- pca-coursera/load_data.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:89168e3c1935287f7c337a68a6eaef985254408e389c8867f77c1aeaf0d4e8bd
-size 408
--- pca-coursera/submission.py
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:bf78fb78fac3893d2510c10c8a8db50ad618f7831d035e6e3665160d672684f6
-size 2309
